913706cb46d329585c56d41a542bda1456c4dd96
Justin Dearden <deardenj@uwindsor.ca>
2019-07-31 14:16:13 -0400

    Made it more Pythonic

--- Chapter-1/2_isPermutation.py
+++ Chapter-1/2_isPermutation.py
@@ -1,21 +1,20 @@
 # Determine whether or not one string is a permutation of another.
 
-def is_permutation(str1, str2):
-  counter = Counter()
-  for letter in str1:
-    counter[letter] += 1
-  for letter in str2:
-    if not letter in counter:
-      return False
-    counter[letter] -= 1
-    if counter[letter] == 0:
-      del counter[letter]
-  return len(counter) == 0
+from collections import Counter
 
-class Counter(dict):
-  def __missing__(self, key):
-    return 0
+def check_permutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    counter = Counter()
+    for c in str1:
+        counter[c] += 1
+        print(c)
+    for c in str2:
+        if counter[c] == 0:
+            return False
+        counter[c] -= 1
+    return True
 
 if __name__ == "__main__":
   import sys
-  print(is_permutation(sys.argv[-2], sys.argv[-1]))
+  print(check_permutation(sys.argv[-2], sys.argv[-1]))
